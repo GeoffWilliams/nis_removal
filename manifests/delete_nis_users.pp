8811d84c22b44d265485e4914492ff4cce153119
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-07 16:44:01 +0800

    fix attempting to write /etc/shadow on aix

--- manifests/delete_nis_users.pp
+++ manifests/delete_nis_users.pp
@@ -15,11 +15,14 @@ class nis_removal::delete_nis_users {
     path  => '/etc/passwd',
   }
 
-  file_line { 'delete NIS users from /etc/shadow':
-    path => '/etc/shadow',
-  }
-
   file_line { 'delete NIS users from /etc/group':
     path => '/etc/group',
+  }
+
+  # no /etc/shadow file on AIX
+  if dig($facts, 'osfamily') != 'AIX'{
+    file_line { 'delete NIS users from /etc/shadow':
+      path => '/etc/shadow',
+    }
   }
 }
--- spec/classes/delete_nis_users_spec.rb
+++ spec/classes/delete_nis_users_spec.rb
@@ -6,4 +6,13 @@ describe 'nis_removal::delete_nis_users' do
   context 'with default values for all parameters' do
     it { should contain_class('nis_removal::delete_nis_users') }
   end
+
+  context 'does not attempt to update /etc/shadow on AIX' do
+    let :facts do
+      {
+        :osfamily => 'AIX',
+      }
+    end
+    it { should_not contain_file_line("delete NIS users from /etc/shadow") }
+  end
 end
